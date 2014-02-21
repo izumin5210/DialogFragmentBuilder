da33fd5542d726b9e2ebfdb318845346120964b4
izumin5210 <contact@izumin.info>
2014-02-21 13:30:41 +0900

    Add updateProgress @ ProgressDialogFragment

--- DialogFragmentBuilder/src/main/java/info/izumin/android/dialogfragmentbuilder/ProgressDialogFragment.java
+++ DialogFragmentBuilder/src/main/java/info/izumin/android/dialogfragmentbuilder/ProgressDialogFragment.java
@@ -37,44 +37,67 @@ public class ProgressDialogFragment extends DialogFragment {
     private static final String KEY_ICON = "key_icon",
             KEY_TITLE_ID = "key_title_id", KEY_TITLE = "key_title",
             KEY_MESSAGE = "key_message", KEY_STYLE = "key_progress_style",
-            KEY_INDETERMINATE = "key_indeterminate",
-            KEY_CANCELABLE = "key_cancelable";
+            KEY_INDETERMINATE = "key_indeterminate", KEY_CANCELABLE = "key_cancelable",
+            KEY_MAX = "key_max", KEY_PROGRESS = "key_progress";
+
+    private ProgressDialog mProgressDialog;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        ProgressDialog dialog = new ProgressDialog(getActivity());
+        mProgressDialog = new ProgressDialog(getActivity());
         Bundle args = getArguments();
 
-        if (args.containsKey(KEY_ICON)) dialog.setIcon(args.getInt(KEY_ICON));
+        if (args.containsKey(KEY_ICON)) mProgressDialog.setIcon(args.getInt(KEY_ICON));
 
         if (args.containsKey(KEY_TITLE_ID)) {
-            dialog.setTitle(args.getInt(KEY_TITLE_ID));
+            mProgressDialog.setTitle(args.getInt(KEY_TITLE_ID));
         } else if (args.containsKey(KEY_TITLE)) {
-            dialog.setTitle(args.getString(KEY_TITLE));
+            mProgressDialog.setTitle(args.getString(KEY_TITLE));
         }
 
         if (args.containsKey(KEY_MESSAGE)) {
-            dialog.setMessage(args.getString(KEY_MESSAGE));
+            mProgressDialog.setMessage(args.getString(KEY_MESSAGE));
         }
 
         if (args.containsKey(KEY_STYLE)) {
-            dialog.setProgressStyle(args.getInt(KEY_STYLE));
+            mProgressDialog.setProgressStyle(args.getInt(KEY_STYLE));
         }
 
-        dialog.setIndeterminate(args.getBoolean(KEY_INDETERMINATE));
+        if (args.containsKey(KEY_MAX)) {
+            mProgressDialog.setMax(args.getInt(KEY_MAX));
+        }
+
+        if (args.containsKey(KEY_PROGRESS)) {
+            mProgressDialog.setProgress(args.getInt(KEY_PROGRESS));
+        }
+
+        mProgressDialog.setIndeterminate(args.getBoolean(KEY_INDETERMINATE));
         this.setCancelable(args.getBoolean(KEY_CANCELABLE));
 
-        return dialog;
+        return mProgressDialog;
+    }
+
+    @Override
+    public ProgressDialog getDialog() {
+        return mProgressDialog;
+    }
+
+    /**
+     * Set the current progress to the specified value. Does not do anything if the progress bar is in indeterminate mode.
+     * @param value the new progress, between 0 and getMax()
+     */
+    public void updateProgress(int value) {
+        mProgressDialog.setProgress(value);
     }
 
     public static class Builder {
 
-        private int iconId, titleId, progressStyle;
+        private int iconId, titleId, progressStyle, max, progress;
         private String title, message;
         private boolean indeterminate = true, cancelable = true;
 
-        private boolean hasIcon, hasTitle, hasMessage, hasStyle;
+        private boolean hasIcon, hasTitle, hasMessage, hasStyle, hasMax, hasProgress;
 
         /**
          * Set the resource id of the Drawable to be used in the title.
@@ -152,6 +175,28 @@ public class ProgressDialogFragment extends DialogFragment {
         }
 
         /**
+         * Set the range of the progress bar to 0...max.
+         * @param max the upper range of this progress bar
+         * @return This Builder object to allow for chaining of calls to set methods.
+         */
+        public Builder setMax(int max) {
+            this.max = max;
+            hasMax = true;
+            return this;
+        }
+
+        /**
+         * Set the current progress to the specified value. Does not do anything if the progress bar is in indeterminate mode.
+         * @param value the new progress, between 0 and getMax()
+         * @return This Builder object to allow for chaining of calls to set methods.
+         */
+        public Builder setProgress(int value) {
+            this.progress = progress;
+            hasProgress = true;
+            return this;
+        }
+
+        /**
          * Create a SupoprtAlertDialogFragment with the arguments supplied to this builder.
          * @return Created AlertDialogFragment object.
          */
@@ -168,8 +213,9 @@ public class ProgressDialogFragment extends DialogFragment {
             }
 
             if (hasMessage) args.putString(KEY_MESSAGE, message);
-
             if (hasStyle) args.putInt(KEY_STYLE, progressStyle);
+            if (hasMax) args.putInt(KEY_MAX, max);
+            if (hasProgress) args.putInt(KEY_PROGRESS, progress);
 
             args.putBoolean(KEY_INDETERMINATE, indeterminate);
             args.putBoolean(KEY_CANCELABLE, cancelable);
